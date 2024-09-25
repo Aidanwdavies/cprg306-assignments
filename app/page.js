195dde012174d903ba6eb66029d2aaaa312f9cf7
Aidanwdavies <147774136+Aidanwdavies@users.noreply.github.com>
2024-09-24 21:47:40 -0600

    Cleaned project up
    Commit 3

---
+++ app/page.js
@@ -0,0 +1,11 @@
+export default function Page() {
+    return (
+      <div>
+        <h1>Week 3 Demo</h1>
+        <p>
+          This is a demo of a simple shopping list app.
+        </p>
+        <ShoppingList />
+      </div>
+    );
+  }
