d42a76bb9badb3ff7689b173613bd03c825fd527
Aidanwdavies <147774136+Aidanwdavies@users.noreply.github.com>
2024-09-24 23:13:57 -0600

    update

--- app/page.js
+++ app/page.js
@@ -1,10 +1,15 @@
+import Link from "next/link";
+
 export default function Page() {
     return (
       <div>
-        <h1>Week 3 Demo</h1>
+        <h1>Week 2 Demo</h1>
         <p>
           This is a demo of a simple shopping list app.
         </p>
+        <li>
+         <link href="/week-2/student-info">week-2/student-info</link>
+        </li>
       </div>
     );
   }
---
+++ app/week-2/student-info.js
@@ -0,0 +1,16 @@
+
+
+export default function StudentInfo() {
+  return (
+    <div>
+      <h1>Aidan Davies</h1>
+      <p>
+        Check out my GitHub repository: 
+        <Link href="https://github.com/Aidanwdavies/cprg306-assignments.git">aidan Git
+        </Link>
+      </p>
+    </div>
+  );
+};
+
+
